09ce760bf6f45dd2b196dedf9245ab54f318f901
Olegzandr VD <alex@oresoftware.com>
2018-09-01 21:18:19 -0700

    pdev:'set'

--- cli/ores-git-squash-to-integration.sh
+++ cli/ores-git-squash-to-integration.sh
@@ -15,6 +15,8 @@ git add .
 git add -A
 git commit --allow-empty -am "ores/gitflow auto-commit (PRE-squashed)"
 
+base=`git merge-base --fork-point HEAD "remotes/origin/dev"`
+
 git fetch origin;
 # GIT_MERGE_AUTOEDIT=no git merge origin;
 # git merge --no-commit --no-edit origin;
@@ -22,12 +24,11 @@ git fetch origin;
 #git merge --no-edit origin;
 git merge --no-edit 'HEAD@{upstream}';
 
-
 current_commit="$(git rev-parse HEAD)"
 new_branch="$current_branch@squashed";
 
-git checkout -b "$new_branch";
-git reset --soft "remotes/origin/dev";
+git checkout --no-track -b "$new_branch";
+git reset --soft "$base";
 
 git add .
 git add -A
@@ -36,5 +37,25 @@ git commit --allow-empty -am "ores/gitflow auto-commit (squashed)"
 clean_branch=`echo "$current_branch" | tr -dc '[:alnum:]'`  # replace non-alpha-numerics with nothing
 git config --local "branch.$clean_branch.orescommit" "$current_commit"
 
-git push -u origin "$new_branch"
+git push -u origin "$new_branch" || {
+  echo "Could not push to remote.";
+  exit 1;
+}
+
+if ! type -f waldo &> /dev/null; then
+ npm i -s -g waldo || {
+   echo "Could not install waldo/find command line utility.";
+   exit 1;
+ }
+fi
+
+
+echo "Successfully pushed, now checking out new feature branch.";
+
+ores_checkout
+
+# now we want to make all the files non-writable
+#waldo -n node_modules -n '\.git' -n .idea | xargs chmod -w
+
+
 
