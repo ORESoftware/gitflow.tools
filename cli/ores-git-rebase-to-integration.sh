71473795132dde617f0dc64846665e52a4ba17bd
Alexander Mills <alex@oresoftware.com>
2018-09-19 13:58:12 -0700

    ores/auto-commit => 'set'

--- cli/ores-git-rebase-to-integration.sh
+++ cli/ores-git-rebase-to-integration.sh
@@ -7,8 +7,9 @@ current_branch="$(git rev-parse --abbrev-ref HEAD)"
 if [ "$current_branch" == "master" ] || [ "$current_branch" == "dev" ]; then
     echo 'Aborting script because you are on master or dev branch; you need to be on a feature branch.';
     git pull;
-    exit $?;
+    exit $?;  # use 0 not 1
 fi
+
 
 time_seconds=`node -e 'console.log(String(Date.now()).slice(0,-3))'`;
 git fetch origin;
@@ -17,8 +18,8 @@ git add .
 git add -A
 git commit --allow-empty -am "merge_at_${time_seconds}"
 
-git merge -Xignore-all-space "remotes/origin/dev" # use --no-ff to force a new commit
-git merge -Xignore-all-space --no-edit 'HEAD@{upstream}';
+git rebase -Xignore-all-space "remotes/origin/dev"
+git rebase -Xignore-all-space --no-edit 'HEAD@{upstream}';
 
 git push origin HEAD
 
--- cli/ores-git-sqush-rebase-to-integration.sh
+++ cli/ores-git-sqush-rebase-to-integration.sh
@@ -10,7 +10,7 @@ if [ -z "$commit_message" ]; then
   exit 1;
 fi
 
-export current_branch=`git rev-parse --abbrev-ref HEAD`
+export current_branch="$(git rev-parse --abbrev-ref HEAD)"
 
 if [[ "$current_branch" != */feature/* ]] ; then
   echo "Current branch does not seem to be a feature branch by name, please check, and use --force to override.";
@@ -27,28 +27,22 @@ git merge -Xignore-all-space --no-edit 'HEAD@{upstream}';
 
 base="remotes/origin/dev";
 
-current_commit="$(git rev-parse HEAD)"
+current_commit=`git rev-parse HEAD`
 new_branch="$current_branch@squashed";
 
 git branch -D "$new_branch" 2> /dev/null || {
   echo "(no branch named '$new_branch' to delete)";
 }
 
-fork_point=`git merge-base --fork-point origin/dev`;
-
-if [ -z "$fork_point" ]; then
-   echo 'Could not find fork-point with origin/dev';
-   exit 1;
-fi
-
 git checkout --no-track -b "$new_branch";
-git reset --soft "$fork_point";
+git rebase -Xignore-all-space "$base";
+
+git reset --soft "$base";
 
 git add .
 git add -A
 git commit --allow-empty -am "ores gitflow auto-commit (squashed): $commit_message"
 
-git rebase -Xignore-all-space "$base";
 
 clean_branch=`echo "$current_branch" | tr -dc '[:alnum:]'`  # replace non-alpha-numerics with nothing
 git config --local "branch.$clean_branch.orescommit" "$current_commit"
